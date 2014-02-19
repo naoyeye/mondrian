753456029302f3c678b844da652d33521ae2b55c
J.Y Han <ihanjiyun@gmail.com>
2014-02-19 18:08:39 +0800

    add files

---
+++ assets/js/frame.js
@@ -0,0 +1,14 @@
+/* 
+* @Author: hanjiyun
+* @Date:   2014-02-19 15:49:52
+* @Last Modified by:   hanjiyun
+* @Last Modified time: 2014-02-19 16:24:34
+*/
+
+var div = document.createElement('div');
+
+div.innerHTML = '<div id="mondrian-backhome" style="position:fixed;top:10px;left:10px;"><a href="/" style="font-size:20px;color:#fff;font-family:serif;z-index:999;background:#333332;height:40px; width:40px;line-height:40px; text-align:center; display:inline-block;text-decoration:none">&lt;</a></div>'
+
+document.body.appendChild(div)
+
+// document.head.appendChild(div)
