28ca2bac3788109dee69d01e714b98d6464dd575
J.Y Han <ihanjiyun@gmail.com>
2014-02-25 18:30:48 +0800

    add demo 4, improve nav jump.

--- assets/js/frame.js
+++ assets/js/frame.js
@@ -2,12 +2,22 @@
 * @Author: hanjiyun
 * @Date:   2014-02-19 15:49:52
 * @Last Modified by:   hanjiyun
-* @Last Modified time: 2014-02-19 16:24:34
+* @Last Modified time: 2014-02-25 18:04:34
 */
 
 var div = document.createElement('div');
 
-div.innerHTML = '<div id="mondrian-backhome" style="position:fixed;top:10px;left:10px;"><a href="/" style="font-size:20px;color:#fff;font-family:serif;z-index:999;background:#333332;height:40px; width:40px;line-height:40px; text-align:center; display:inline-block;text-decoration:none">&lt;</a></div>'
+var href = window.location.href;
+var link;
+
+if(href.indexOf('demo') > -1){
+    link = '#demo';
+} else if(href.indexOf('site') > -1){
+    link = '#site';
+}
+
+
+div.innerHTML = '<div id="mondrian-backhome" style="position:fixed;top:10px;left:10px;"><a href="/'+ link +'" style="font-size:20px;color:#fff;font-family:serif;z-index:999;background:#333332;height:40px; width:40px;line-height:40px; text-align:center; display:inline-block;text-decoration:none">&lt;</a></div>'
 
 document.body.appendChild(div)
 
